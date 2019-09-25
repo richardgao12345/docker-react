c6d2a7cf97e3ea57859b2796428db74401b7642f
Richard Gao <richardgao@Richards-MacBook-Pro.local>
2019-09-25 10:28:02 -0700

    Initial commit and base application.

---
+++ build/precache-manifest.eb0370e5514c41e05ab43fa120c5b8eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f6e830cbadd93c9716628211fe6f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff0a02b1b51f66e5354",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "63c07cabe1dbd37e39dc",
+    "url": "/static/js/2.3660b280.chunk.js"
+  },
+  {
+    "revision": "fff0a02b1b51f66e5354",
+    "url": "/static/js/main.bfc76c96.chunk.js"
+  },
+  {
+    "revision": "09e27f03fdb94530568a",
+    "url": "/static/js/runtime-main.9d4f7790.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
